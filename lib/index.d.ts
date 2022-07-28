6b9256a519753252bce44ac568b5adbc255c919c
Raghu Saxena <poiasdpoiasd@live.com>
2022-07-29 01:03:38 +0800

    Fix types file

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
-export default {
-    // todo: stricter typing?
-    encode: (data: any, buffer?: Buffer, offest?: number) => Buffer,
-    decode: (data: Buffer | string, start?: number, end?: number, encoding?: any) => any,
-}
+declare const bencode: {
+    encode(data: any, buffer?: Buffer, offest?: number): Buffer;
+    decode(data: Buffer | string, start?: number, end?: number, encoding?: any): any;
+};
+
+export default bencode;
