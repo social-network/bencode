b00a8d44725cdf3241f6748946c43b00472a31fe
Raghu Saxena <poiasdpoiasd@live.com>
2022-07-29 00:44:06 +0800

    bump up package json

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,5 @@
 export default {
     // todo: stricter typing?
-    encode(data: any, buffer?: Buffer, offest?: number): Buffer,
-
-    decode(data: Buffer | string, start?: number, end?: number, encoding?: any): any;
+    encode: (data: any, buffer?: Buffer, offest?: number) => Buffer,
+    decode: (data: Buffer | string, start?: number, end?: number, encoding?: any) => any,
 }
